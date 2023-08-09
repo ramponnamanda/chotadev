39a08b99dd89c16e9a642b2965915c6a077ae930
Sitaramaswamy Ponnamanda <psrswami92@gmail.com>
2023-08-09 14:16:23 +0000

    ToDoApp initial commit

---
+++ todoapp/src/main/java/com/chotadev/todoapp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.chotadev.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
